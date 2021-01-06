2e6f84f66619aa3b88eb91c06b176659002f6811
unknown <you@example.com>
2021-01-06 22:13:23 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let readlineSync = require("readline-sync");
+let number1 = readlineSync.question("Give first number:  ");
+let number2 = readlineSync.question("Give second number: ");
+let number3 = readlineSync.question("Give third number: ");
+let number4 = readlineSync.question("Give fourth number: ");
+let number5 = readlineSync.question("Give fifth number: ");
+
+if (number1 > 0) {
+  console.log("Positive");
+} else if (number1 < 0) {
+  console.log("Negative");
+}
+if (number2 > 0) {
+  console.log("Positive");
+} else if (number2 < 0) {
+  console.log("Negative");
+}
+if (number3 > 0) {
+  console.log("Positive");
+} else if (number3 < 0) {
+  console.log("Negative");
+}
+if (number4 > 0) {
+  console.log("Positive");
+} else if (number4 < 0) {
+  console.log("Negative");
+}
+if (number5 > 0) {
+  console.log("Positive");
+} else if (number5 < 0) {
+  console.log("Negative");
+}
